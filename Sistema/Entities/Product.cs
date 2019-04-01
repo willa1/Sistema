a2de706cbb3965b0cb3b76a82209064a302c73af
Willa Cantanhede <willacantanhede@gmail.com>
2019-04-01 16:30:07 -0300

    Sistema mega simples para cadastro e calculo de produto

---
+++ Sistema/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Sistema.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
